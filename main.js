a54e5ae256e7d32b2699df4b57b1b16709304534
oshi2005 <vohuutri.nt1994@gmail.com>
2022-08-30 09:23:29 +0700

    Home Work Counter

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var count = 0 ;
+var count1 = 0 ;
+function f1() {
+    document.getElementById("btn").innerHTML=count;
+    count++;
+}
+function f2() {
+    document.getElementById("btn1").innerHTML=count1;
+    count1--;
+}
